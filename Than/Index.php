885de6c197662411c89c22eb6a4b4773e99df76c
jonathanmaristela11 <maristelajonathan1@gmail.com>
2026-07-16 17:18:13 +0800

    Add files via upload

---
+++ Than/Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <form action="process.php" method="GET"> -->
+    <form action="process.php" method="GET">
+        <label for="">name:</label>
+        <input type="text" name="Fullname" id=""> <br>
+        <label for="">email:"></label>
+        <input type="email" name="email" id=""> <br>
+        <label for="">message:</label>
+        <textarea name="message" id=""></textarea> <br>
+        <input type="submit" value="Send message"> 
+
+    </form>
+</body>
+</html>
---
+++ Than/process.php
@@ -0,0 +1,15 @@
+<?php
+// $name = $_POST["Fullname"];
+// $email = $_POST["email"];
+// $message = $_POST["message"];
+
+$name = $_GET["Fullname"];
+$email = $_GET["email"];
+$message = $_GET["Message"];
+
+echo "Welcome $name <br>";
+echo "email $email <br>";
+echo "message $message <br>";
+
+
+?>
